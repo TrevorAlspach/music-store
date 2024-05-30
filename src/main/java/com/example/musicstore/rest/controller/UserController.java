05ab708af475d84121d3fdf962fe8995176c7785
Trevor Alspach <trevoralspach@gmail.com>
2024-05-30 18:06:48 -0400

    spotify refresh token

--- src/main/java/com/example/musicstore/rest/controller/UserController.java
+++ src/main/java/com/example/musicstore/rest/controller/UserController.java
@@ -4,19 +4,20 @@ import com.example.musicstore.entities.User;
 import com.example.musicstore.repositories.UserRepository;
 import com.example.musicstore.rest.dto.SpotifyRefreshTokenDTO;
 import com.example.musicstore.services.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/user")
 public class UserController {
+
+    private static final Logger logger = LoggerFactory.getLogger(UserController.class);
 
     @Autowired
     private UserService userService;
@@ -31,15 +32,18 @@ public class UserController {
         User authenticatedUser = (User) authentication.getPrincipal();
         SpotifyRefreshTokenDTO tokenDTO = new SpotifyRefreshTokenDTO();
         tokenDTO.setToken(authenticatedUser.getSpotifyRefreshToken());
+        logger.info("get token is" + tokenDTO.getToken());
         return ResponseEntity.ok(tokenDTO);
     }
 
     @PostMapping("/spotifyRefreshToken")
-    ResponseEntity<SpotifyRefreshTokenDTO> updateSpotifyRefreshToken(Authentication authentication, SpotifyRefreshTokenDTO tokenDTO){
+    ResponseEntity<SpotifyRefreshTokenDTO> updateSpotifyRefreshToken(Authentication authentication, @RequestBody SpotifyRefreshTokenDTO tokenDTO){
         User authenticatedUser = (User) authentication.getPrincipal();
         authenticatedUser.setSpotifyRefreshToken(tokenDTO.getToken());
         User updatedUser = userService.updateUser(authenticatedUser);
-        return ResponseEntity.ok().build();
+        tokenDTO.setToken(updatedUser.getSpotifyRefreshToken());
+        logger.info("updated token is" + updatedUser.getSpotifyRefreshToken());
+        return ResponseEntity.ok(tokenDTO);
     }
 
 
